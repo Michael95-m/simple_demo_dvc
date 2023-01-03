fab8e8919d2d5bd5950469ef1bdee7204056b883
Michael95-m <nygma.19101995@gmail.com>
2023-01-03 10:34:49 +0630

    hot fix about forgetting index=False in to_csv function

--- src/stages/data_split.py
+++ src/stages/data_split.py
@@ -13,11 +13,14 @@ def data_split(config_path: Text) -> None:
     logger = getlogger("DATA_SPLIT")
 
     df = pd.read_csv(config["featurize"]["features_path"])
+    logger.debug(f"Original num of columns: {len(df.columns)}")
     logger.info("Split the data")
     train, test = train_test_split(df, test_size=config["data_split"]["test_size"], random_state=config["base"]["random_state"])
     logger.info("Save train and test sets")
-    train.to_csv(config["data_split"]["trainset_path"])
-    test.to_csv(config["data_split"]["testset_path"])
+    logger.debug(f"Num of columns for train: {len(train.columns)}")
+    logger.debug(f"Num of columns for test: {len(train.columns)}")
+    train.to_csv(config["data_split"]["trainset_path"], index=False)
+    test.to_csv(config["data_split"]["testset_path"], index=False)
 
 if __name__ == "__main__":
     args_parser = argparse.ArgumentParser()
